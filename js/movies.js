c47bfc5e010d7c28c827f99a4b0dd8d35720f67c
Romero Alejo <alerom2206@gmail.com>
2024-05-20 17:26:32 -0300

    agrego funcion para que aparezca el título al posicionar el mouse en la película

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,6 @@
 let apiKey = '44d03bcbc2bc43b1c81acd014f727c91';
 let paginaActual =  1;
+
 function llamarApi(){
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${paginaActual}`)
         .then(response => response.json())
@@ -7,11 +8,15 @@ function llamarApi(){
 }
 
 function dibujarDatos(json){
-    const filas1 = json.results.map(objeto => filaTendencia(objeto)); 
-    document.getElementById('peliculas').innerHTML = filas1;
+    const filas1 = json.results.map(objeto => filaTendencia(objeto));
+    document.getElementById('peliculas').innerHTML = filas1.join('');
     const filas2 = json.results.map(objeto => filaAclamada(objeto));
-    document.getElementById('aclamadas').innerHTML = filas2;
-
+    document.getElementById('aclamadas').innerHTML = filas2.join('');
+    
+    // Añadir event listeners a las imágenes de tendencia
+    agregarEventListenersTooltip(document.querySelectorAll('#peliculas img'));
+    // Añadir event listeners a las imágenes aclamadas
+    agregarEventListenersTooltip(document.querySelectorAll('#aclamadas img'));
 }
 
 function filaTendencia(objeto){
@@ -19,21 +24,21 @@ function filaTendencia(objeto){
     return`
     <div class="pelicula">
         <a href="film.html">
-            <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}">
-            <div class="nombre">${objeto.title}</div>
-        </a>
+            <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}" data-title="${objeto.title}">
+            <div id="tooltip" class="nombre"></div>
+            </a>
     </div>
     `;
-   }
+}
 
-   function filaAclamada(objeto){
+function filaAclamada(objeto){
     const baseUrl = 'https://image.tmdb.org/t/p/w200'; 
     return`
     <div>
-    <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}">
+        <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}" data-title="${objeto.title}">
     </div>
     `;
-   }
+}
 
 function siguientePagina(){
     paginaActual++;
@@ -41,10 +46,32 @@ function siguientePagina(){
 }
 
 function anteriorPagina(){
-    if(paginaActual>1){
+    if(paginaActual > 1){
         paginaActual--;
     }
     llamarApi();
+}
+
+function agregarEventListenersTooltip(imagenes) {
+    const tooltip = document.getElementById('tooltip');
+
+    imagenes.forEach(img => {
+        img.addEventListener('mouseover', (event) => {
+            tooltip.textContent = event.target.dataset.title;
+            tooltip.style.display = 'block';
+            tooltip.style.left = `${event.pageX + 10}px`;
+            tooltip.style.top = `${event.pageY + 10}px`;
+        });
+
+        img.addEventListener('mousemove', (event) => {
+            tooltip.style.left = `${event.pageX + 10}px`;
+            tooltip.style.top = `${event.pageY + 10}px`;
+        });
+
+        img.addEventListener('mouseout', () => {
+            tooltip.style.display = 'none';
+        });
+    });
 }
 
 llamarApi();
