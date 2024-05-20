7eb283aa256c30ed59d3a61af04d48b62adbb047
Romero Alejo <alerom2206@gmail.com>
2024-05-20 10:01:30 -0300

    agrego api en sección aclamada, hago los botones funcionales en sección tendencia y arreglo texto de títulos

--- js/movies.js
+++ js/movies.js
@@ -1,24 +1,56 @@
+let apiKey = '44d03bcbc2bc43b1c81acd014f727c91';
+let paginaActual =  1;
 function llamarApi(){
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=44d03bcbc2bc43b1c81acd014f727c91')
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=${paginaActual}`)
         .then(response => response.json())
         .then(data => dibujarDatos(data));
 }
 
 function dibujarDatos(json){
-    const filas = json.results.map(objeto => fila(objeto)); 
-    document.getElementById('peliculas').innerHTML = filas;
+    const filas1 = json.results.map(objeto => filaTendencia(objeto)); 
+    document.getElementById('peliculas').innerHTML = filas1;
+    const filas2 = json.results.map(objeto => filaAclamada(objeto));
+    document.getElementById('aclamadas').innerHTML = filas2;
+
 }
 
-function fila(objeto){
-    const baseUrl = 'https://image.tmdb.org/t/p/w500'; 
+function filaTendencia(objeto){
+    const baseUrl = 'https://image.tmdb.org/t/p/w300'; 
     return`
     <div class="pelicula">
         <a href="film.html">
-            <img src="${baseUrl}${objeto.poster_path}" alt="">
+            <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}">
             <div class="nombre">${objeto.title}</div>
         </a>
     </div>
     `;
+   }
+
+   function filaAclamada(objeto){
+    const baseUrl = 'https://image.tmdb.org/t/p/w200'; 
+    return`
+    <div>
+    <img src="${baseUrl}${objeto.poster_path}" alt="${objeto.title}">
+    </div>
+    `;
+   }
+
+function siguientePagina(){
+    paginaActual++;
+    llamarApi();
+}
+
+function anteriorPagina(){
+    if(paginaActual>1){
+        paginaActual--;
+    }
+    llamarApi();
 }
 
 llamarApi();
+
+const btnSiguiente = document.getElementById('btnSiguiente');
+const btnAnterior = document.getElementById('btnAnterior');
+
+btnSiguiente.addEventListener('click', siguientePagina);
+btnAnterior.addEventListener('click', anteriorPagina);
